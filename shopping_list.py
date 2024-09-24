67c22f754adc2438b33ccc1d7b2ff05b0554fdd9
Monika Szydłak <mnkkuc3@gmial.com>
2024-09-24 20:14:39 +0200

    Add total product count

--- shopping_list.py
+++ shopping_list.py
@@ -9,6 +9,9 @@ for sklep, produkty in zakupy.items():
     sklep_formatted = sklep.title()
     produkty_formatted = [produkt.title() for produkt in produkty]
     print(f"Idę do {sklep_formatted}, kupuję tam {produkty_formatted}.")
+    suma_produktów += len(produkty)
+
+print(f"W sumie kupuję {suma_produktów} produktów.")
 
 
-    
+
