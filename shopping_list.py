1c7522719ee188715bb9d3ae35cf98809328cba9
Monika Szydłak <mnkkuc3@gmial.com>
2024-09-24 20:04:18 +0200

    Iterate over the shopping list and print

--- shopping_list.py
+++ shopping_list.py
@@ -5,3 +5,10 @@ zakupy = {
     "warzywniak": ["marchew", "seler", "rukola"]
 }
 
+for sklep, produkty in zakupy.items():
+    sklep_formatted = sklep.title()
+    produkty_formatted = [produkt.title() for produkt in produkty]
+    print(f"Idę do {sklep_formatted}, kupuję tam {produkty_formatted}.")
+
+
+    
