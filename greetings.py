eb67101c81f4790862e9b9ed19982d717c3569ad
Monika Szydłak <mnkkuc3@gmial.com>
2024-09-30 20:50:29 +0200

    Add greetings

---
+++ greetings.py
@@ -0,0 +1,4 @@
+def greetings():
+    print("Przesyłam pozdrowienia dla mojego mentora Maćka!")
+    print("Pozdrowienia dla mojego programistycznego mistrza! Dzięki Tobie już prawie nie grozi mi wywalenie systemu jednym kliknięciem. Kodu przybywa, bugów ubywa! 😄💻!")
+    
