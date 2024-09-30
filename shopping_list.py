72a93787b1d7d6ebe4871a6cee7ea8b37ee8a963
Monika Szydłak <mnkkuc3@gmial.com>
2024-09-30 20:04:26 +0200

    The whole project

--- shopping_list.py
+++ shopping_list.py
@@ -5,11 +5,14 @@ zakupy = {
     "warzywniak": ["marchew", "seler", "rukola"]
 }
 
+suma_produktów = 0
+
 for sklep, produkty in zakupy.items():
     sklep_formatted = sklep.title()
     produkty_formatted = [produkt.title() for produkt in produkty]
     print(f"Idę do {sklep_formatted}, kupuję tam {produkty_formatted}.")
-    suma_produktów += len(produkty)
+
+    suma_produktów += len(produkty_formatted)
 
 print(f"W sumie kupuję {suma_produktów} produktów.")
 
